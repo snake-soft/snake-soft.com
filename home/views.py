c1197774c55ddb497db91d667a308addb4768d5f
Snake-Soft <info@snake-soft.com>
2020-01-15 01:11:44 +0100

    created user and loginrequired

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Home(TemplateView):
-    template_name = 'index.html'
+class Home(LoginRequiredMixin, TemplateView):
+    template_name = 'home/home.html'
+    
+    #===========================================================================
+    # def get(self, request, *args, **kwargs):
+    #     response = TemplateView.get(self, request, *args, **kwargs)
+    #     from django.contrib import messages
+    #     messages.add_message(request, messages.INFO, 'Hello world.')
+    #     return response
+    #===========================================================================
